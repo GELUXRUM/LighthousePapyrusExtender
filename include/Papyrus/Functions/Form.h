878fd83e169796ace1ca91356e706ba81667ebdc
Geluxrum Lux <geluxrum@gmail.com>
2023-09-20 19:50:57 +0100

    1.6.0 / 1.7.0 stuff

--- include/Papyrus/Functions/Form.h
+++ include/Papyrus/Functions/Form.h
@@ -14,7 +14,27 @@ namespace Papyrus::Form
 		a_form->formFlags &= ~a_flag;
 	}
 
-	std::uint32_t GetFormType(IVM& a_vm, VMStackID a_stackID, std::monostate,
+	inline bool IsContainerFlagSet(IVM& a_vm, VMStackID a_stackID, std::monostate,
+		RE::TESForm* a_form,
+		std::uint32_t a_flag)
+	{
+		if (!a_form) {
+			a_vm.PostError("Form is None", a_stackID, Severity::kError);
+			return false;
+		}
+
+		if (a_form->GetFormType() != RE::ENUM_FORM_ID::kCONT) {
+			a_vm.PostError("Form is not of CONT type", a_stackID, Severity::kError);
+			return false;
+		}
+
+		auto thisContainer = (RE::TESObjectCONT*)a_form;
+
+		//(a_form->formFlags & a_flag) != 0
+		return (thisContainer->flags & a_flag) != 0;
+	}
+
+	inline std::uint32_t GetFormType(IVM& a_vm, VMStackID a_stackID, std::monostate,
 		RE::TESForm* a_form)
 	{
 		if (!a_form) {
@@ -25,7 +45,7 @@ namespace Papyrus::Form
 		return static_cast<std::uint32_t>(a_form->GetFormType());
 	}
 
-	std::string GetFormEditorID(IVM& a_vm, VMStackID a_stackID, std::monostate,
+	inline std::string GetFormEditorID(IVM& a_vm, VMStackID a_stackID, std::monostate,
 		RE::TESForm* a_form)
 	{
 		if (!a_form) {
@@ -35,6 +55,75 @@ namespace Papyrus::Form
 
 		return a_form->GetFormEditorID();
 	}
+
+	inline std::vector<RE::BGSConstructibleObject*> GetParentCOBJs(IVM& a_vm, VMStackID a_stackID, std::monostate,
+		RE::TESForm* a_form,
+		RE::BGSKeyword* a_workshopKeyword,
+		RE::BGSKeyword* a_categoryKeyword)
+	{
+		std::vector<RE::BGSConstructibleObject*> result;
+
+		if (!a_form) {
+			a_vm.PostError("Form is None", a_stackID, Severity::kError);
+			return result;
+		}
+
+		if (auto dataHandler = RE::TESDataHandler::GetSingleton(); dataHandler) {
+			auto cobjArray = dataHandler->GetFormArray<RE::BGSConstructibleObject>();
+			for (auto thisCOBJ : cobjArray) {
+				if (thisCOBJ->createdItem == a_form) {
+					// if keyword is not nullptr we check if it's present on the form and 
+					if (a_categoryKeyword != nullptr && thisCOBJ->filterKeywords.HasKeyword(a_categoryKeyword) == false) {
+						continue;
+					} else if (a_workshopKeyword != nullptr && thisCOBJ->benchKeyword != a_workshopKeyword) {
+						continue;
+					}
+
+					result.push_back(thisCOBJ);
+				}
+			}
+		}
+
+		return result;
+	}
+
+	inline RE::BGSKeyword* GetWorkbenchKeyword(IVM& a_vm, VMStackID a_stackID, std::monostate,
+		RE::TESForm* a_form)
+	{
+		if (!a_form) {
+			a_vm.PostError("Form is None", a_stackID, Severity::kError);
+			return nullptr;
+		}
+
+		if (a_form->GetFormType() != RE::ENUM_FORM_ID::kCOBJ) {
+			a_vm.PostError("Form is not of COBJ type", a_stackID, Severity::kError);
+			return nullptr;
+		}
+
+		RE::BGSKeyword* workshopKW = ((RE::BGSConstructibleObject*)a_form)->benchKeyword;
+
+		if (!workshopKW) {
+			a_vm.PostError("COBJ has no Workbench Keyword", a_stackID, Severity::kError);
+			return nullptr;
+		} else {
+			return workshopKW;
+		}
+	}
+
+	/*
+	inline RE::TESForm* GetMagicEffectAssociatedForm(IVM& a_vm, VMStackID a_stackID, std::monostate,
+		RE::EffectSetting* a_magicEffect)
+	{
+		if (!a_magicEffect) {
+			a_vm.PostError("MagicEffect is None", a_stackID, Severity::kError);
+			return nullptr;
+		}
+		
+		RE::TESForm* associatedForm = a_magicEffect->data.associatedForm;
+		
+		return (associatedForm == nullptr) ? nullptr : associatedForm;
+	}
+	*/
 
 	inline bool IsDynamicForm(IVM& a_vm, VMStackID a_stackID, std::monostate,
 		RE::TESForm* a_form)
@@ -93,6 +182,10 @@ namespace Papyrus::Form
 		a_vm.BindNativeMethod("Lighthouse", "ClearRecordFlag", ClearRecordFlag, true);
 		a_vm.BindNativeMethod("Lighthouse", "GetFormType", GetFormType, true);
 		a_vm.BindNativeMethod("Lighthouse", "GetFormEditorID", GetFormEditorID, true);
+		a_vm.BindNativeMethod("Lighthouse", "GetParentCOBJs", GetParentCOBJs, true);
+		a_vm.BindNativeMethod("Lighthouse", "GetWorkbenchKeyword", GetWorkbenchKeyword, true);
+		//a_vm.BindNativeMethod("Lighthouse", "GetMagicEffectAssociatedForm", GetMagicEffectAssociatedForm, true);
+		a_vm.BindNativeMethod("Lighthouse", "IsContainerFlagSet", IsContainerFlagSet, true);
 		a_vm.BindNativeMethod("Lighthouse", "IsDynamicForm", IsDynamicForm, true);
 		a_vm.BindNativeMethod("Lighthouse", "IsFormInMod", IsFormInMod, true);
 		a_vm.BindNativeMethod("Lighthouse", "IsRecordFlagSet", IsRecordFlagSet, true);
