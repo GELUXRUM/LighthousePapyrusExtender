b220855c53c500da64cfb66adee8c97990bcb838
Geluxrum Lux <geluxrum@gmail.com>
2023-09-27 22:53:40 +0100

    Add definition for function arguments Will update functions to use this once I have the rest of 1.7.0 finished

--- include/Common.h
+++ include/Common.h
@@ -6,4 +6,6 @@ namespace Papyrus
 	using IVM = RE::BSScript::IVirtualMachine;
 	using VMStackID = std::uint32_t;
 	using Severity = RE::BSScript::ErrorLogger::Severity;
+
+	#define ARGS [[maybe_unused]] IVM& a_vm, [[maybe_unused]] VMStackID a_stackID, std::monostate
 }
