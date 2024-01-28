f620463e49f2c2b14b45c7da142d75ba70533d3f
Geluxrum Lux <geluxrum@gmail.com>
2024-01-28 23:42:02 +0000

    Add Cell name getter/setter

--- include/Papyrus/Functions/Cell.h
+++ include/Papyrus/Functions/Cell.h
@@ -13,6 +13,17 @@ namespace Papyrus::Cell
 		return !a_cell->GetCantWaitHere();
 	}
 
+	inline std::string GetCellName(IVM& a_vm, VMStackID a_stackID, std::monostate,
+		RE::TESObjectCELL* a_cell)
+	{
+		if (!a_cell) {
+			a_vm.PostError("Cell is None", a_stackID, Severity::kError);
+			return "";
+		}
+
+		return a_cell->fullName.c_str();
+	}
+
 	inline bool IsExterior(IVM& a_vm, VMStackID a_stackID, std::monostate,
 		RE::TESObjectCELL* a_cell)
 	{
@@ -24,10 +35,26 @@ namespace Papyrus::Cell
 		return a_cell->IsExterior();
 	}
 
+	inline void SetCellName(IVM& a_vm, VMStackID a_stackID, std::monostate,
+		RE::TESObjectCELL* a_cell,
+		std::string a_name)
+	{
+		if (!a_cell) {
+			a_vm.PostError("Cell is None", a_stackID, Severity::kError);
+			return;
+		}
+
+		a_cell->fullName = a_name;
+
+		return;
+	}
+
 	inline void Bind(IVM& a_vm)
 	{
 		a_vm.BindNativeMethod("Lighthouse", "GetCanWaitInCell", GetCanWaitInCell, true);
+		a_vm.BindNativeMethod("Lighthouse", "GetCellName", GetCellName, true);
 		a_vm.BindNativeMethod("Lighthouse", "IsExterior", IsExterior, true);
+		a_vm.BindNativeMethod("Lighthouse", "SetCellName", SetCellName, true);
 
 		logger::info("Cell functions registered.");
 	}
